108b6630ad7d726e09bc16f461ce1f191bc64550
arion1993 <petrovskiarion@gmail.com>
2023-12-01 12:23:37 +0000

    Add cat breed object and random generator number function to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let catBreedInfo = {
+    breed: ['Persian', 'Siamese', 'Snowshoe', 'Turkish Van', 'Ragdoll', 'Maine Coon'],
+    name: ['Odysseus', 'Athena', 'Poseidon', 'Pegasus', 'Apollo'],
+    ability: ['Opening the door', 'Skateboarding', 'Swimming', 'Very sharp claws', 'High jumps']
+}
+
+let getRandomNumber = num => {
+    return Math.floor(Math.random() * num)
+}
